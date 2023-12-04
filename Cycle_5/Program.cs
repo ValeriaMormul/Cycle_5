dbb50d5dcae02e666dca0561f13578a0005e15d8
Valeriia Mormul <152411242+ValeriaMormul@users.noreply.github.com>
2023-12-05 00:31:51 +0200

    Добавьте файлы проекта.

---
+++ Cycle_5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int sumOfPrimes = 0;
+        for (int i = 2; i <= 50; i++)
+        {
+            if (IsPrime(i))
+            {
+                sumOfPrimes += i;
+            }
+        }
+        Console.WriteLine($"Сумма простых чисел от 1 до 50: {sumOfPrimes}");
+    }
+    static bool IsPrime(int number)
+    {
+        if (number < 2)
+        {
+            return false;
+        }
+        for (int i = 2; i <= Math.Sqrt(number); i++)
+        {
+            if (number % i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
